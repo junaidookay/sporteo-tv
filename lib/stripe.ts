2581c804d2b0b4b625fc1668b870ea4be67a0ee3
Junaid Ookay <junaid.ookay@gmail.com>
2026-04-07 04:04:02 +0500

    chore: add better error message for missing stripe key

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,4 +2,10 @@ import 'server-only'
 
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '')
+const secretKey = process.env.STRIPE_SECRET_KEY
+
+if (!secretKey) {
+  throw new Error('STRIPE_SECRET_KEY environment variable is not set')
+}
+
+export const stripe = new Stripe(secretKey)
